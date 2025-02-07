fc051c623467b92d57dd49b47f24160f973f8dd4
buddy0225 <whisky@kakao.com>
2025-02-07 23:40:36 +0900

    DB 데이터 출력

--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -1,16 +1,27 @@
 package com.apple.shop;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemRepository itemRepository;
 
     @GetMapping("/list")
     String list (Model model) {
+        List<Item> result = itemRepository.findAll();
+        System.out.println(result.get(0).price);
+        System.out.println(result.get(0).title);
+
+        ArrayList<Object> a = new ArrayList<>();
+
         model.addAttribute("name", "홍길동");
         return "list.html";
     }
