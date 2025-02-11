c05a50f682d4587ad1afacea5cb4d320f9cf26f8
buddy0225 <whisky@kakao.com>
2025-02-11 23:47:51 +0900

    상품 추가기능 1(Map 자료형)

--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -4,8 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,5 +22,15 @@ public class ItemController {
         model.addAttribute("items", result);
 
         return "list.html";
+    }
+
+    @GetMapping("/write")
+    String write() {
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    String writePost(@RequestParam Map<String, Object> formData) {
+        return "redirect:/list";
     }
 }
