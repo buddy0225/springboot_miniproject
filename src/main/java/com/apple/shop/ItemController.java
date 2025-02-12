1cfe978f9330e438d4efd3fe78c0979c2a18d428
buddy0225 <whisky@kakao.com>
2025-02-12 22:23:19 +0900

    상품 추가기능 2 & Navbar 만들기

--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -4,11 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -30,7 +29,8 @@ public class ItemController {
     }
 
     @PostMapping("/add")
-    String writePost(@RequestParam Map<String, Object> formData) {
+    String writePost(@ModelAttribute Item item) {
+        itemRepository.save(item);
         return "redirect:/list";
     }
 }
